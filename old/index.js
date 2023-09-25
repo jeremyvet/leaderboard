53c2769aaffa358dcdae02e89805ff92a9583bf1
Jeremy <91553695+DonCarbone1234@users.noreply.github.com>
2023-09-25 13:25:38 -0500

    rermoved old code

--- old/index.js
+++
@@ -1,7 +0,0 @@
-fetch("http://150.220.144.108:1337/score").then((res) => {
-  res.json().then((json) => {
-    document.getElementById("table").innerHTML += json.map((item, i) => {
-      return `<tr><td>${item.name}</td><td>${item.score}</td><td>${i+1}</td></tr>`;
-    });
-  });
-});
